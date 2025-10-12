5ba4b2f12da32832346953fa765105939830dab8
uwx <maxine@cybergal.com>
2025-10-12 20:35:36 +0200

    Increase stmt timeout to 500ms

--- init-db/02-init-db.sh
+++ init-db/02-init-db.sh
@@ -41,5 +41,5 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly_user;
     -- ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO readonly_user;
 
-    ALTER ROLE readonly_user SET statement_timeout = '250ms';
+    ALTER ROLE readonly_user SET statement_timeout = '500ms';
 EOSQL
