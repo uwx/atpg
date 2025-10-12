8b309ee894e046ed1abdcc7e4ed340937166ca6a
uwx <maxine@cybergal.com>
2025-10-12 23:10:56 +0200

    Replace kysely with rawdogged SQL

--- node/index.ts
+++ node/index.ts
@@ -1,27 +1,8 @@
-import { JSONColumnType, Kysely, PostgresDialect, RawBuilder, sql } from 'kysely';
 import { Pool } from 'pg';
 import { Jetstream } from "@skyware/jetstream";
 
-const dialect = new PostgresDialect({
-    pool: new Pool({
-        connectionString: process.env.DATABASE_URL,
-    })
-});
-
-interface Database {
-    atproto_records: {
-        did: string;
-        rkey: string;
-        nsid: string;
-        cid: string;
-        record: JSONColumnType<object>;
-        indexed_at: Date;
-        updated_at: Date;
-    };
-}
-
-const db = new Kysely<Database>({
-    dialect,
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
 });
 
 const date24HoursAgo = new Date(Date.now() - 24 * 60 * 60 * 1000);
@@ -46,50 +27,32 @@ jetstream.on('commit', async ({commit, did}) => {
         // console.log('Processing commit:', commit, 'for DID:', did);
         switch (commit.operation) {
             case 'create':
-                await db
-                    .insertInto('atproto_records')
-                    .values({
-                        did,
-                        rkey: commit.rkey,
-                        nsid: commit.collection,
-                        cid: commit.cid,
-                        record: json(commit.record),
-                        indexed_at: sql<any>`${new Date()}::timestamptz`,
-                        updated_at: sql<any>`${new Date()}::timestamptz`,
-                    })
-                    .onConflict(oc => oc.doNothing())
-                    .execute();
+                await pool.query(/*sql*/`
+                INSERT INTO atproto_records (
+                    did, rkey, nsid, cid, record, indexed_at, updated_at
+                ) VALUES ($1, $2, $3, $4, $5::jsonb, NOW(), NOW())
+                    ON CONFLICT DO NOTHING;`,
+                [did, commit.rkey, commit.collection, commit.cid, JSON.stringify(commit.record)]
+                );
             case 'update':
                 // note: this will throw if the original record does not exist in the database. i need to figure out
                 // a clean solution to upsert 
-                await db
-                    .updateTable('atproto_records')
-                    .set({
-                        did,
-                        rkey: commit.rkey,
-                        nsid: commit.collection,
-                        cid: commit.cid,
-                        record: json(commit.record),
-                        updated_at: sql<any>`${new Date()}::timestamptz`,
-                    })
-                    .where('rkey', '=', commit.rkey)
-                    .where('nsid', '=', commit.collection)
-                    .where('did', '=', did)
-                    .execute();
+                await pool.query(/*sql*/`
+                UPDATE atproto_records
+                    SET did = $1, rkey = $2, nsid = $3, cid = $4, record = $5::jsonb, updated_at = NOW()
+                    WHERE rkey = $2 AND nsid = $3 AND did = $1;`,
+                [did, commit.rkey, commit.collection, commit.cid, JSON.stringify(commit.record)]
+                );
                 break;
             case 'delete':
-                await db.deleteFrom('atproto_records')
-                    .where('rkey', '=', commit.rkey)
-                    .where('nsid', '=', commit.collection)
-                    .where('did', '=', did)
-                    .execute();
+                await pool.query(/*sql*/`
+                DELETE FROM atproto_records
+                    WHERE rkey = $1 AND nsid = $2 AND did = $3;`,
+                [commit.rkey, commit.collection, did]
+                );
                 break;
         }
     } catch (error) {
         console.error('Error processing commit:', error, 'at cursor:', jetstream.cursor);
     }
 })
-
-function json<T>(object: T): RawBuilder<any> {
-    return sql<any>`cast(${object} as jsonb)`
-}
